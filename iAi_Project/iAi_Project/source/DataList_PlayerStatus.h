818e53bdfa52bd59eeb9eb8c31c260285751337f
Komazawa Fusuke <fuusuke0304@gmail.com>
2025-01-26 00:10:15 +0900

    マージミスの修正

--- iAi_Project/iAi_Project/source/DataList_PlayerStatus.h
+++ iAi_Project/iAi_Project/source/DataList_PlayerStatus.h
@@ -52,28 +52,6 @@ class DataList_PlayerStatus : public DataListBase
 		float	fGetPlayerMaxFallSpeed()		{ return this->fPlayerMaxFallSpeed; }		// プレイヤーの最大落下速度取得
 		int		iGetPlayerMaxJumpCount()		{ return this->iPlayerMaxJumpCount; }		// プレイヤーのジャンプ回数(最大数)取得
 
-		/* 2025.01.10 菊池雅道 移動関連の関数追加 開始*/
-		
-		//移動（歩き・走り等）
-		VECTOR	vecGetPlayerOldVector() { return this->vecPlayerOldVector; }			//プレイヤーの移動時のベクトルを取得（ジャンプ慣性等に使用）
-		float	fGetPlayerOldRadian() { return this->fPlayerOldRadian; }			//プレイヤーの移動時の角度を取得（方向転換時等に使用）
-		int		iGetPlayerNormalDashFlameCount() { return this->iPlayerNormalDashFlameCount; }  //通常ダッシュ時経過フレーム数を取得（高速ダッシュへの移行に使用）
-		
-		//ジャンプ
-		int		iGetPlayerJumpCount() { return this->iPlayerJumpCount; }			//プレイヤージャンプ回数を取得
-		bool	bGetPlayerJumpingFlag() { return this->bPlayerJumpingFlag; }			//プレイヤーがジャンプ中かのフラグを取得
-		
-		//回避
-		bool	bGetPlayerDodgingFlag() { return this->bPlayerDodgingFlag; }						//プレイヤーが回避中かのフラグを取得
-		float	fGetPlayerDodgeProgress() { return this->fPlayerDodgeProgress; }					// プレイヤー回避モーション進行率を取得 
-		VECTOR	vecGetPlayerDodgeDirection() { return this->vecPlayerDodgeDirection; }				// プレイヤー回避方向を取得
-		int		iGetPlayerDodgeWhileJumpingCount() { return this->iPlayerDodgeWhileJumpingCount; }	//プレイヤージャンプ中の回避回数を取得
-		bool	bGetPlayerAfterDodgeFlag() { return this->bPlayerAfterDodgeFlag; }					//プレイヤーの回避後フラグを取得
-		float	fGetPlayerDodgeSpeed() { return this->fPlayerDodgeSpeed; }			//プレイヤー回避速度を取得
-		int		iGetPlayerNowDodgeFlame() { return this->iPlayerNowDodgeFlame; }	// プレイヤーの現在の回避フレーム数を取得
-		
-		/* 2025.01.10 菊池雅道 移動関連の関数追加 終了*/
-
 		// カメラ関連
 		int		iGetCameraMode()							{ return this->iCameraMode; }							// カメラモード取得
 		VECTOR	vecGetCameraUp()							{ return this->vecCameraUp; }							// カメラの上方向取得
@@ -118,9 +96,9 @@ class DataList_PlayerStatus : public DataListBase
 		void	SetPlayerDodgeSpeed(float fPlayerDodgeSpeed)				{ this->fPlayerDodgeSpeed = fPlayerDodgeSpeed; }				//プレイヤー回避速度を取得
 		void	SetPlayerNowDodgeFlame(int iPlayerNowDodgeFlame)			{ this->iPlayerNowDodgeFlame = iPlayerNowDodgeFlame; }			// プレイヤー回避時間を取得
 		/* 2025.01.10 菊池雅道 移動関連の関数追加 終了 */
-		/* 2025.01.22 菊池雅道 攻撃関連の変数追加開始  */
-		void	SetPlayerChargeAttakTargetPoint(VECTOR vecPlayerChargeAttakTargetPoint)		{ this->vecPlayerChargeAttakTargetPoint = vecPlayerChargeAttakTargetPoint; }	////プレイヤー溜め攻撃の方向を設定
-		/* 2025.01.22菊池雅道 移動関連の関数追加 終了 */
+		/* 2025.01.22 菊池雅道 攻撃関連の変数追加 開始 */
+		void	SetPlayerChargeAttakTargetPoint(VECTOR vecPlayerChargeAttakTargetPoint)		{ this->vecPlayerChargeAttakTargetPoint	= vecPlayerChargeAttakTargetPoint; }	//プレイヤー溜め攻撃の方向を設定
+		/* 2025.01.22 菊池雅道 移動関連の関数追加 終了 */
 		
 		// カメラ関連
 		void	SetCameraMode(int iCameraMode)										{ this->iCameraMode							= iCameraMode; }				// カメラモード設定
@@ -165,11 +143,9 @@ class DataList_PlayerStatus : public DataListBase
 		int		iPlayerDodgeWhileJumpingCount;	//プレイヤージャンプ中の回避回数
 		bool	bPlayerAfterDodgeFlag;			//プレイヤーの回避後フラグ
 		/* 2025.01.09 菊池雅道 移動関連の変数追加終了 */
-		/* 2025.01.22 菊池雅道 攻撃関連の変数追加開始 */
-		VECTOR  vecPlayerChargeAttakTargetPoint;	//プレイヤー溜め攻撃の目的地
-		VECTOR  vecPlayerChargeAttakVector;			//プレイヤー溜め攻撃の方向
-		int		iPlayerNowAttakChargeFlame;			//現在のプレイヤー溜め攻撃チャージフレーム数
-		/* 2025.01.22 菊池雅道 攻撃関連の変数追加終了 */
+		/* 2025.01.22 菊池雅道 攻撃関連の変数追加 開始 */
+		VECTOR	vecPlayerChargeAttakTargetPoint;	//プレイヤー溜め攻撃の方向を取得
+		/* 2025.01.22 菊池雅道 移動関連の関数追加 終了 */
 		
 		/* 能力値関連 */
 		float	fPlayerMoveAcceleration;	// プレイヤーの移動加速度
