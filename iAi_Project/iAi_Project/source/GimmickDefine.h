a1081502e6f35f7d221478d937480bcae7ce856f
kikuchimasamichi <tomcat.mk58@gmail.com>
2025-02-12 19:51:48 +0900

    遠距離攻撃（構え）の遷移作成

---
+++ iAi_Project/iAi_Project/source/GimmickDefine.h
@@ -0,0 +1,8 @@
+/* 2024.02.10 石川智也 ファイル作成 */
+
+#pragma once
+
+
+
+static const int GIMMICK_DISAPPEAR_TIME = 180;			// ギミックの消滅時間
+static const int GIMMICK_TEXTURE_CHANGE_COUNT = 180;	// ギミックテクスチャ変更カウント
